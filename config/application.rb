19fef47e9e7c14e9c8c2607ea5027527f59b25d7
Richard <richard.wigley@gmail.com>
2021-02-18 19:50:43 +0000

    Scaffold prevented from creating css assets

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module Mugshots
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.stylesheets false
+    end
   end
 end
