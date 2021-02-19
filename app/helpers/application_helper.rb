5e1cd3816b4d7d55ea31b8d67dbbd02f366bcf1e
Richard <richard.wigley@gmail.com>
2021-02-19 09:53:30 +0000

    Application helper renders svg within inline wrapper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def render_svg(name, styles: "", title: nil)
+    filename = "media/images/#{name}.svg"
+    title ||= name.underscore.humanize
+    inline_svg_pack_tag(filename, aria: true, nocomment: true, title: title, class: styles)
+  end
 end
